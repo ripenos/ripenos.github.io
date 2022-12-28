d7731c74a5d79fff8e99a1b624dd827d1f9b6c35
RGOS <120778877+ripenos@users.noreply.github.com>
2022-12-28 20:46:37 +0530

    Add files via upload

---
+++ Docs/frameworks/scriptFW.js
@@ -0,0 +1,11 @@
+function bg(){
+    document.body.style.background= "url('https://pbs.twimg.com/media/FUrfhkyXsAwgIof?format=jpg&name=4096x4096')";
+    document.getElementById("bgC").style.display= "none";
+    document.getElementById("ClC").style.display= "block";
+}
+
+function cl(){
+    document.body.style.background= "linear-gradient(0deg, rgb(82, 41, 0) 0%, rgb(77, 37, 0) 50%, rgb(82, 41, 0) 100%)";
+    document.getElementById("bgC").style.display= "block";
+    document.getElementById("ClC").style.display= "none";
+}
