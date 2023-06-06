8c5c07e9d96a43b0c63b4ea8ea2a77ef9dc04e84
RGOS <120778877+ripenos@users.noreply.github.com>
2023-06-06 17:36:03 +0530

    Add files via upload

---
+++ Ripenos/index.js
@@ -0,0 +1,9 @@
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("service_worker.js").then(registration=>{
+      console.log("SW Registered!");
+    }).catch(error=>{
+      console.log("SW Registration Failed");
+    });
+}else{
+  console.log("Not supported");
+}
